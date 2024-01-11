c24718fb94246267e2541c9828bce377fd1399e2
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2024-01-11 09:14:50 -0300

    Adding firsts styles and components to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
+
 const App = () => {
   return (
     <div>
-      <p>Hello, world!</p>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 };
